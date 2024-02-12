ca5ebb4689773eb46d12813a74b4d3935eba35d3
0Jumpero <nudy121@wp.pl>
2024-02-12 19:06:54 +0100

    Offline caching fixes

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,43 @@
+// Cache name and resources to cache
 const CACHE_NAME = 'sudoku-v1';
+const RES = [
+  '.',
+  '/favicon.ico',
+  'index.html',
+  'script.js',
+  'style.css',
+  'manifest.json',
+  'service-worker.js',
+  'img/icon-72.png',
+  'img/icon-128.png',
+  'img/icon-144.png',
+  'img/icon-192.png',
+  'img/icon-512.png',
+  'img/icon.png'
+];
 
 // Install the service worker and cache resources
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/script.js',
-        '/style.css',
-        '/manifest.json',
-        '/service-worker.js',
-        '/img/icon-72.png',
-        '/img/icon-128.png',
-        '/img/icon-144.png',
-        '/img/icon-192.png',
-        '/img/icon-512.png',
-        '/img/icon.png'
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(RES)));
 });
 
 // Activate the service worker and clean up old caches
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if(cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.map((cacheName) => {
+    if(cacheName !== CACHE_NAME) {
+      return caches.delete(cacheName);
+    }
+  }))));
+});
+
+// Fetch intercept to load resources from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if(cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
   );
 });
